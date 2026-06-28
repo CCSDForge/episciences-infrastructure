7ba85d94384bf5bcfd7b676eb1205a8459fb0405
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2026-06-28 11:48:32 +0200

    cleanup monorepo attempt

--- apps/episciences-citations/docker/apache/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Optionally, include the vhost in the main configuration
-if ! grep -q "Include conf/extra/episciences-citations.conf" /usr/local/apache2/conf/httpd.conf; then
-    echo "Include conf/extra/episciences-citations.conf" >> /usr/local/apache2/conf/httpd.conf
-fi
-
-#chown -R www-data:www-data /var/www/data /var/www/cache /var/www/logs
-exec "$@"
-
